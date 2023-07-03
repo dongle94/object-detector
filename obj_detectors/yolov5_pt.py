c90355223fc687908064083ce7e3723427e3fae8
DY. Seo <35318158+dongle94@users.noreply.github.com>
2023-07-03 18:00:12 +0900

    Merge Yolo detector  (#2)
    * Feat: Yolov5 to inference class
    
    * Doc: 가장 최소한의 yolov5 install module
    
    * Doc: Yolo Detector class
    
    * Feat: Yolo Detector class
    - Yolo class code exception 처리
    
    * Feat: Yolo Detector class
    - test code 출력 정리 및 클래스 코드 가독성 개선
    
    * doc: test image add
    
    * Feat: Add Configuration module
    
    * Docs: Write Weights Download README
    
    * Feat: Add Medialoader
    - 동영상, rtsp(스트리밍), 웹캠(0, 1 등) 사용 가능
    
    * Style: yolov5_pt.py
    - modify code convention
    
    * Style: object detector class
    원하는 소스를 sys.argv 로 주고 디텍션 추론 가능 우선 yolo class 적용

---
+++ obj_detectors/yolov5_pt.py
@@ -0,0 +1,137 @@
+import sys
+import copy
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+
+from pathlib import Path
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+os.chdir(ROOT)
+
+from models.yolo import check_img_size, letterbox, non_max_suppression, scale_boxes
+
+
+class YoloDetector(nn.Module):
+    def __init__(self, weight='yolov5s.pt', device: int or str = "cpu", img_size=640, fp16=False, auto=True, fuse=True):
+        super().__init__()
+
+        device = "cpu" if device == "" else device
+        self.device = self.select_device(device)
+        self.cuda = torch.cuda.is_available() and device
+        self.fp16 = True if fp16 and self.device.type != "cpu" else False
+        _model = attempt_load(weight, device=device, inplace=True, fuse=fuse)
+        _model.half() if self.fp16 else _model.float()
+        self.model = _model
+        self.stride = max(int(_model.stride.max()), 32)
+        self.img_size = check_img_size(img_size, s=self.stride)
+        self.names = _model.module.names if hasattr(_model, 'module') else _model.names  # get class names
+        self.auto = auto
+
+    def warmup(self, imgsz=(1, 3, 640, 640)):
+        _im = torch.empty(*imgsz, dtype=torch.half if self.fp16 else torch.float, device=self.device)  # input
+        self.forward(_im)  # warmup
+        print("-- Yolov5 Detector warmup --")
+
+    def preprocess(self, _img):
+        _im = letterbox(_img, self.img_size, stride=self.stride, auto=self.auto)[0]  # padded resize
+        _im = _im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        _im = np.ascontiguousarray(_im)  # contiguous
+
+        _im = torch.from_numpy(_im).to(self.device)
+        _im = _im.half() if self.fp16 else _im.float()  # uint8 to fp16/32
+        _im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(_im.shape) == 3:
+            _im = torch.unsqueeze(_im, dim=0)     # expand for batch dim
+        return _im, _img
+
+    def forward(self, _im):
+        if self.fp16 and _im.dtype != torch.float16:
+            _im = _im.half()  # to FP16
+        y = self.model(_im)
+
+        if isinstance(y, (list, tuple)):
+            return self.from_numpy(y[0]) if len(y) == 1 else [self.from_numpy(x) for x in y]
+        else:
+            return self.from_numpy(y)
+
+    @staticmethod
+    def postprocess(pred, im_shape, im0_shape, max_det=100):
+        pred = non_max_suppression(pred, classes=[0], max_det=max_det)[0]
+        det = scale_boxes(im_shape[2:], copy.deepcopy(pred[:, :4]), im0_shape).round()
+        det = torch.cat([det, pred[:, 4:]], dim=1)
+
+        return pred, det
+
+    def from_numpy(self, x):
+        return torch.from_numpy(x).to(self.device) if isinstance(x, np.ndarray) else x
+
+    @staticmethod
+    def select_device(device=''):
+        device = str(device).strip().lower().replace('cuda', '').replace('none', '')
+        cpu = device == 'cpu'
+        if cpu:
+            os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+        elif device:  # non-cpu device requested
+            os.environ[
+                'CUDA_VISIBLE_DEVICES'] = device  # set environment variable - must be before assert is_available()
+
+        if not cpu and torch.cuda.is_available():
+            device = device if device else '0'
+            arg = 'cuda:0'
+        else:
+            arg = 'cpu'
+
+        return torch.device(arg)
+
+
+def attempt_load(weight, device=None, inplace=True, fuse=True):
+
+    ckpt = torch.load(str(weight), map_location='cpu')
+    ckpt = (ckpt.get('ema') or ckpt['model']).to(device).float()
+
+    # Model compatibility updates
+    if not hasattr(ckpt, 'stride'):
+        ckpt.stride = torch.tensor([32.])
+    if hasattr(ckpt, 'names') and isinstance(ckpt.names, (list, tuple)):
+        ckpt.names = dict(enumerate(ckpt.names))  # convert to dict
+    ckpt = ckpt.fuse().eval() if fuse and hasattr(ckpt, 'fuse') else ckpt.eval()
+
+    from models.yolo import Detect, Model
+    # Module compatibility updates
+    for m in ckpt.modules():
+        t = type(m)
+        if t in (nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU, Detect, Model):
+            m.inplace = inplace  # torch 1.7.0 compatibility
+            if t is Detect and not isinstance(m.anchor_grid, list):
+                delattr(m, 'anchor_grid')
+                setattr(m, 'anchor_grid', [torch.zeros(1)] * m.nl)
+        elif t is nn.Upsample and not hasattr(m, 'recompute_scale_factor'):
+            m.recompute_scale_factor = None  # torch 1.11.0 compatibility
+
+    # return model
+    return ckpt
+
+
+if __name__ == "__main__":
+    model = YoloDetector(weight='./weights/yolov5s6.pt', device=0, img_size=1280)
+    model.warmup()
+
+    import cv2
+    img = cv2.imread('./data/images/army.jpg')
+    im, im0 = model.preprocess(img)
+
+    _pred = model.forward(im)
+    _pred, _det = model.postprocess(_pred, im.shape, im0.shape)
+    for d in _det:
+        print(f"box:{int(d[0]), int(d[1]), int(d[2]), int(d[3])}, class: {model.names[int(d[5])]}, conf: {d[4]:.3f}")
+
+        x1, y1, x2, y2 = map(int, d[:4])
+        cv2.rectangle(im0, (x1, y1), (x2, y2), (128, 128, 255), thickness=2, lineType=cv2.LINE_AA)
+        cv2.putText(im0, f"{model.names[int(d[-1])]}: {d[-2]:.2f}", (x1, y1+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 0, 255), 1)
+
+    cv2.imshow('result', im0)
+    cv2.waitKey(0)

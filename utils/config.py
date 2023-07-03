b3db3cb0a21b72dc01035a3938dc0cbbf1bc4172
dongle94 <sdy1029@gmail.com>
2023-07-03 17:35:48 +0900

    Feat: Add Configuration module

---
+++ utils/config.py
@@ -0,0 +1,41 @@
+from yacs.config import CfgNode as CN
+
+
+_C = CN()
+
+# Envrionments
+_C.DEVICE = None
+
+# Object Detector
+_C.IMG_SIZE = 640
+_C.DET_MODEL_TYPE = ""
+_C.DET_MODEL_PATH = ""
+_C.HALF = False
+
+# Logger
+_C.LOG_LEVEL = 'DEBUG'
+_C.CONSOLE_LOG = False
+_C.LOGGER_NAME = ""
+_C.FILE_LOG = False
+_C.LOG_FILE_DIR = './log/'
+_C.LOG_FILE_SIZE = 100
+_C.LOG_FILE_COUNTER = 10
+_C.LOG_FILE_ROTATE_TIME = "D"
+_C.LOG_FILE_ROTATE_INTERVAL = 1
+
+
+def update_config(cfg, args):
+    if not args:
+        print("-- No exist Config File --")
+        return
+
+    cfg.defrost()
+    cfg.merge_from_file(args)
+
+    cfg.freeze()
+
+
+if __name__ == "__main__":
+    import sys
+    with open(sys.argv[1], 'w') as f:
+        print(_C, file=f)

cd46fbde08ed6e9507b2e5b7ca6da07d6f191667
dongle94 <sdy1029@gmail.com>
2023-08-10 15:27:28 +0900

    Feat: dv1 annotation 도구 - `-i` 옵션으로 이미지 디렉토리 경로(1개 폴더 경로), `-j` 옵션으로 json 파일 경로, `-c` 옵션으로 클래스 인덱스 (아마도 1-17 사이) - 원본이미지 크기가 FHD 이상이면 반씩 이미지를 줄여서 작은 이미지에 라벨링   - 원본 이미지 크기와 조정된 이미지 크기를 가지고 조정된 이미지에 점을 찍어 바운딩 박스를 구성하고 원본이미지 크기의 좌표로 변환하여 라벨링 - 두 점을 찍고 스페이스바를 누르면 하나의 제품 이미지 라벨링 완료

--- dv/dv1_annotate.py
+++ dv/dv1_annotate.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 datavoucher 1 - 다올씨앤디
 Product object 17 classes
@@ -7,9 +8,9 @@ import json
 import os
 import sys
 import argparse
-from datetime import datetime
-# from collections import defaultdict
 import cv2
+from datetime import datetime
+from collections import defaultdict
 import shutil
 
 from pathlib import Path
@@ -24,129 +25,189 @@ from utils.config import update_config
 from utils.logger import get_logger, init_logger
 
 
+PROD_CLASS = {
+    "background": 0,
+    "닭윙1.3kg": 1,
+    "시즈닝100g": 2,
+    "시즈닝500g": 3,
+    "닭볶음용1.3kg": 4,
+    "닭다리살1kg": 5,
+    "닭갈비간장500g": 6,
+    "닭볶음탕용600g": 7,
+    "허니버터소스1kg": 8,
+    "소떡소떡소스1kg": 9,
+    "닭갈비고추장500g": 10,
+    "갈릭디핑소스500g": 11,
+    "치즈치폴레소스500g": 12,
+    "13cls": 13,
+    "14cls": 14,
+    "15cls": 15,
+    "16cls": 16,
+    "17cls": 17,
+}
+
+
+img = None
+mouseX, mouseY = 0, 0
+box_point = []
+
+
+def draw_event(event, x, y, flags, param):
+    global mouseX, mouseY, img, box_point
+    if event == cv2.EVENT_LBUTTONDOWN:
+        mouseX, mouseY = x, y
+        cv2.circle(img, (x, y), 5, (32, 216, 32), -1)
+        box_point.append((x, y))
+        if len(box_point) == 2:
+            cv2.rectangle(img, box_point[0], box_point[1], (32, 32, 216), 2, cv2.LINE_AA)
+        cv2.imshow(param, img)
+    if event == cv2.EVENT_MOUSEMOVE:
+        im = img.copy()
+        img_size = im.shape
+        cv2.line(im, (x, 0), (x, img_size[0]), (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.line(im, (0, y), (img_size[1], y), (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.imshow(param, im)
+
 def main(opt=None):
-    get_logger().info("Start dv1 annotation script.")
+    get_logger().info(f"Start dv1 annotation script. Object class is {opt.class_num}")
     IMGS_DIR = opt.imgs_dir
     get_logger().info(f"Input Directory is {IMGS_DIR}")
 
-    # detector = ObjectDetector(cfg=cfg)
-    # obj_classes = defaultdict(int)
-    #
-    # if os.path.exists(opt.json_file):
-    #     with open(opt.json_file, 'r') as file:
-    #         basic_fmt = json.load(file)
-    #     get_logger().info(f"{opt.json_file} is exist. append annotation file.")
-    #     img_ids = 0
-    #     if len(basic_fmt['images']) != 0:
-    #         img_ids = int(basic_fmt['images'][-1]['id']) + 1
-    #         get_logger().info(f"last image file name: {basic_fmt['images'][-1]['file_name']}")
-    #     anno_ids = 0
-    #     if len(basic_fmt['annotations']) != 0:
-    #         anno_ids = int(basic_fmt["annotations"][-1]['id']) + 1
-    #         for anno in basic_fmt['annotations']:
-    #             obj_classes[int(anno['category_id'])] += 1
-    #         get_logger().info(f"old object classes: {obj_classes}")
-    # else:
-    #     get_logger().info(f"{opt.json_file} is not exist. Create new annotation file")
-    #     basic_fmt = {
-    #         "info": {"year": "2023", "version": "1",
-    #                  "description": "",
-    #                  "contributor": "",
-    #                  "url": "",
-    #                  "date_created": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")},
-    #         "licenses": [{"id": 1, "url": "", "name": "Unknown"}],
-    #         "categories": [{"id": 0, "name": "male", "supercategory": "person"},
-    #                        {"id": 1, "name": "female", "supercategory": "person"}],
-    #         "images": [],
-    #         "annotations": []
-    #     }
-    #     img_ids = 0
-    #     anno_ids = 0
-    #
-    # is_out = False
-    #
-    # IMGS = os.listdir(IMGS_DIR)
-    # IMGS.sort()
-    # for i in IMGS:
-    #     if is_out is True:
-    #         break
-    #     if os.path.splitext(i)[-1].lower() not in ['.jpg', '.png', '.jpeg', '.bmp']:
-    #         continue
-    #
-    #     img_file = os.path.join(IMGS_DIR, i)
-    #     get_logger().info(f"process {img_file}.")
-    #     f = cv2.imread(img_file)
-    #
-    #     # yolov5 human detector
-    #     im = detector.preprocess(f)
-    #     _pred = detector.detect(im)
-    #     _pred, _det = detector.postprocess(_pred)
-    #
-    #     img_info = {
-    #         "id": img_ids,
-    #         "license": 1,
-    #         "file_name": i,
-    #         "height": f.shape[0],
-    #         "width": f.shape[1],
-    #         "data_captured": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    #     }
-    #
-    #     # bbox annotation
-    #     c_id = defaultdict(int)
-    #     tmp_annos = []
-    #     for d in _det:
-    #         if d[4] < float(opt.confidence):
-    #             continue
-    #         x1, y1, x2, y2 = map(int, d[:4])
-    #         w, h = x2-x1, y2-y1
-    #         fc = f.copy()
-    #         cv2.rectangle(fc, (x1, y1), (x2, y2), (16, 16, 255), thickness=2, lineType=cv2.LINE_AA)
-    #
-    #         if fc.shape[0] > 1000:
-    #             fc = cv2.resize(fc, (int(fc.shape[1] * 0.8), int(fc.shape[0] * 0.8)))
-    #         cv2.imshow(img_file, fc)
-    #
-    #         category_id = 0
-    #         k = cv2.waitKey(0)
-    #         if k == ord('q'):
-    #             get_logger().info("-- CV2 Stop --")
-    #             is_out = True
-    #             break
-    #         elif k == ord('1'):
-    #             category_id = 0
-    #         elif k == ord('2'):
-    #             category_id = 1
-    #         elif k == ord('d'):
-    #             continue
-    #
-    #         anno_info = {"id": anno_ids,
-    #                      "image_id": img_ids,
-    #                      "category_id": category_id,
-    #                      "bbox": [x1, y1, w, h],
-    #                      "area": w*h,
-    #                      "segmentation": [],
-    #                      "iscrowd": 0}
-    #         tmp_annos.append(anno_info)
-    #         anno_ids += 1
-    #         c_id[category_id] += 1
-    #
-    #     cv2.destroyAllWindows()
-    #     if is_out is False:
-    #         for k, v in c_id.items():
-    #             obj_classes[k] += v
-    #         for _anno_info in tmp_annos:
-    #             basic_fmt["annotations"].append(_anno_info)
-    #
-    #         basic_fmt["images"].append(img_info)
-    #         new_path = os.path.join(IMGS_DIR, 'already', i)
-    #         if not os.path.exists(os.path.dirname(new_path)):
-    #             os.makedirs(os.path.dirname(new_path))
-    #         shutil.move(img_file, new_path)
-    #     img_ids += 1
-    #
-    # with open(os.path.join(opt.json_file), 'w') as outfile:
-    #     json.dump(basic_fmt, outfile, indent=2)
-    # get_logger().info(f"obj classes: {obj_classes}")
+    obj_classes = defaultdict(int)
+
+    if os.path.exists(opt.json_file):
+        with open(opt.json_file, 'r') as file:
+            basic_fmt = json.load(file)
+        get_logger().info(f"{opt.json_file} is exist. append annotation file.")
+        img_ids = 0
+        if len(basic_fmt['images']) != 0:
+            img_ids = int(basic_fmt['images'][-1]['id']) + 1
+            get_logger().info(f"last image file name: {basic_fmt['images'][-1]['file_name']}")
+        anno_ids = 0
+        if len(basic_fmt['annotations']) != 0:
+            anno_ids = int(basic_fmt["annotations"][-1]['id']) + 1
+            for anno in basic_fmt['annotations']:
+                obj_classes[int(anno['category_id'])] += 1
+            get_logger().info(f"old object classes: {obj_classes}")
+    else:
+        get_logger().info(f"{opt.json_file} is not exist. Create new annotation file")
+        basic_fmt = {
+            "info": {"year": "2023", "version": "1",
+                     "description": "datavoucher dataset",
+                     "contributor": "",
+                     "url": "",
+                     "date_created": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")},
+            "licenses": [{"id": 1, "url": "", "name": "Unknown"}],
+            "categories": [
+                {"id": 0, "name": "background", "supercategory": "background"},
+                {"id": 1, "name": "닭윙1.3kg", "supercategory": "product"},
+                {"id": 2, "name": "시즈닝100g", "supercategory": "product"},
+                {"id": 3, "name": "시즈닝500g", "supercategory": "product"},
+                {"id": 4, "name": "닭볶음용1.3kg", "supercategory": "product"},
+                {"id": 5, "name": "닭다리살1kg", "supercategory": "product"},
+                {"id": 6, "name": "닭갈비간장500g", "supercategory": "product"},
+                {"id": 7, "name": "닭볶음탕용600g", "supercategory": "product"},
+                {"id": 8, "name": "허니버터소스1kg", "supercategory": "product"},
+                {"id": 9, "name": "소떡소떡소스1kg", "supercategory": "product"},
+                {"id": 10, "name": "닭갈비고추장500g", "supercategory": "product"},
+                {"id": 11, "name": "갈릭디핑소스500g", "supercategory": "product"},
+                {"id": 12, "name": "치즈치폴레소스500g", "supercategory": "product"},
+                {"id": 13, "name": "14cls", "supercategory": "product"},
+                {"id": 14, "name": "15cls", "supercategory": "product"},
+                {"id": 15, "name": "16cls", "supercategory": "product"},
+                {"id": 16, "name": "17cls", "supercategory": "product"},
+                {"id": 17, "name": "18cls", "supercategory": "product"},
+            ],
+            "images": [],
+            "annotations": []
+        }
+        img_ids = 0
+        anno_ids = 0
+
+    image_extension = ['.jpg', '.png', '.jpeg', '.bmp']
+    IMGS = [i for i in os.listdir(IMGS_DIR) if os.path.splitext(i)[-1].lower() in image_extension]
+    IMGS.sort()
+
+    for idx, i in enumerate(IMGS):
+
+        img_file = os.path.join(IMGS_DIR, i)
+        get_logger().info(f"process {img_file}.")
+        f = cv2.imread(img_file)
+
+        # Connect click event
+        winname = f"{idx+1}/{len(IMGS)}"
+        cv2.namedWindow(winname)
+        cv2.setMouseCallback(winname, draw_event, winname)
+
+        img_info = {
+            "id": img_ids,
+            "license": 1,
+            "file_name": i,
+            "height": f.shape[0],
+            "width": f.shape[1],
+            "data_captured": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        }
+
+        # image resize
+        orig_img_size = (f.shape[0], f.shape[1])
+        edit_img_size = orig_img_size
+        global img
+        img = f
+        while f.shape[0] >= 1080:
+            f = cv2.resize(f, (int(f.shape[1] / 2), int(f.shape[0] / 2)))
+            img = f
+            edit_img_size = (f.shape[0], f.shape[1])
+
+        # winname에 한글 입력 불가
+        cv2.imshow(winname, f)
+        k = cv2.waitKey(0)
+        if k == ord('q'):
+            get_logger().info("-- CV2 Stop --")
+            break
+        elif k == ord(" "):
+            global box_point
+            if len(box_point) == 2:
+                pt1, pt2 = box_point[0], box_point[1]
+                rel_pt1 = (pt1[0]/edit_img_size[1], pt1[1]/edit_img_size[0])
+                rel_pt2 = (pt2[0]/edit_img_size[1], pt2[1]/edit_img_size[1])
+                orig_pt1 = (int(rel_pt1[0] * orig_img_size[1]), int(rel_pt1[1] * orig_img_size[0]))
+                orig_pt2 = (int(rel_pt2[0] * orig_img_size[1]), int(rel_pt2[1] * orig_img_size[0]))
+                w = int(orig_pt2[0] - orig_pt1[0])
+                h = int(orig_pt2[1] - orig_pt1[1])
+                box_point = []
+            else:
+                raise Exception("2 points not clicked!")
+            anno_info = {
+                "id": anno_ids,
+                "image_id": img_ids,
+                "category_id": opt.class_num,
+                "bbox": [orig_pt1[0], orig_pt1[1], w, h],
+                "area": w*h,
+                "segmentation": [],
+                "iscrowd": 0
+            }
+            get_logger().info(
+                f"Save label {img_file}. Box point is {orig_pt1, w, h}. resize box point is {pt1, pt2}."
+                f"relative point is ({rel_pt1[0]:.4f}, {rel_pt1[1]:.4f}) ({rel_pt2[0]:.4f}, {rel_pt2[1]:.4f})"
+            )
+
+            # add annotation
+            basic_fmt["images"].append(img_info)
+            basic_fmt["annotations"].append(anno_info)
+            img_ids += 1
+            anno_ids += 1
+            obj_classes[opt.class_num] += 1
+
+            new_path = os.path.join(IMGS_DIR, 'already', i)
+            if not os.path.exists(os.path.dirname(new_path)):
+                os.makedirs(os.path.dirname(new_path))
+            shutil.move(img_file, new_path)
+
+        cv2.destroyAllWindows()
+
+    with open(os.path.join(opt.json_file), 'w') as outfile:
+        json.dump(basic_fmt, outfile, indent=1)
+    get_logger().info(f"Stop Annotation. Obj classes: {obj_classes}")
 
 
 def args_parse():
@@ -155,6 +216,8 @@ def args_parse():
                         help='image directory path')
     parser.add_argument('-j', '--json_file', required=True,
                         help="if write this file, append annotations")
+    parser.add_argument('-c', '--class_num', required=True, type=int,
+                        help="object class number 1~17")
     _args = parser.parse_args()
     return _args
 

ee25341dd0afd7dd22b27b6adeccf5168fdc98c9
dongle94 <sdy1029@gmail.com>
2023-08-22 15:36:54 +0900

    Feat: dv4 - half auto annotation - 반자동 라벨링 스크립트 작성   - 수동 스크립트의 마우스무빙 이벤트, 마우스 2번 클릭시 라벨링 박스 생성 이벤트 연결   - 최초 1회 박스에 디텍션 결과 바운딩박스 초록색으로 그림   - 해당 박스들을 하나씩 확인하면서 남성, 여성, 버릴객체 체크   - 스페이스바 클릭 시 해당 이미지에 대한 어노테이션 json에 쓰기

--- dv/dv4_halfauto_annotate.py
+++ dv/dv4_halfauto_annotate.py
@@ -12,9 +12,9 @@ import argparse
 from datetime import datetime
 from collections import defaultdict
 import cv2
-from tqdm import tqdm
 import shutil
 import numpy as np
+import platform
 
 from pathlib import Path
 
@@ -28,6 +28,66 @@ from utils.config import _C as cfg
 from utils.config import update_config
 from utils.logger import get_logger, init_logger
 from core.obj_detectors import ObjectDetector
+
+
+img = None
+mouseX, mouseY = 0, 0
+box_point = []
+label_info = []
+
+
+def cvtPoint(x, y, orig_size, cvt_size):
+    orig_h, orig_w = orig_size
+    cvt_h, cvt_w = cvt_size
+
+    new_x = int(x / orig_w * cvt_w)
+    new_y = int(y / orig_h * cvt_h)
+    return new_x, new_y
+
+
+def get_box_point(pt1, pt2):
+    x1, y1 = pt1
+    x2, y2 = pt2
+    new_pt1 = (min(x1, x2), min(y1, y2))
+    new_pt2 = (max(x1, x2), max(y1, y2))
+    return new_pt1, new_pt2
+
+
+def draw_event(event, x, y, flags, param):
+    global mouseX, mouseY, img, box_point, label_info
+    if event == cv2.EVENT_LBUTTONDOWN:
+        mouseX, mouseY = x, y
+        cv2.circle(img, (x, y), 3, (32, 216, 32), -1)
+        box_point.append((x, y))
+        if len(box_point) == 2:
+            box_pt1, box_pt2 = get_box_point(box_point[0], box_point[1])
+            crop_img = img[box_pt1[1]:box_pt2[1], box_pt1[0]:box_pt2[0]]
+            cv2.imshow("crop", crop_img)
+            key = cv2.waitKey(0)
+            _class = 0
+            if key == ord("1"):
+                _class = 1
+                b_color = (216, 16, 16)  # male blue
+            elif key == ord("2"):
+                _class = 2
+                b_color = (16, 16, 224)  # female red
+            elif key == ord("d"):
+                box_point = []
+                cv2.destroyWindow("crop")
+                return
+            else:
+                raise Exception("Wrong Key input")
+            label_info.append((box_pt1, box_pt2, _class))
+            cv2.rectangle(img, box_pt1, box_pt2, b_color, 1, cv2.LINE_AA)
+            box_point = []
+            cv2.destroyWindow("crop")
+            print(label_info)
+    if event == cv2.EVENT_MOUSEMOVE:
+        im = img.copy()
+        img_size = im.shape
+        cv2.line(im, (x, 0), (x, img_size[0]), (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.line(im, (0, y), (img_size[1], y), (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.imshow(param, im)
 
 
 def main(opt=None):
@@ -94,6 +154,8 @@ def main(opt=None):
                 f = cv2.imdecode(arr, cv2.IMREAD_UNCHANGED)
 
             winname = f"{idx + 1}/{len(IMGS)}"
+            cv2.namedWindow(winname)
+            cv2.setMouseCallback(winname, draw_event, winname)
 
             # yolov5 human detector
             im = detector.preprocess(f0)
@@ -115,30 +177,66 @@ def main(opt=None):
 
             f1 = f0.copy()
             im_h, im_w = f0.shape[0], f0.shape[1]
+            _det = [_d for _d in _det if _d[4] >= float(opt.confidence)]
             for d in _det:
-                if d[4] < float(opt.confidence):
-                    continue
+                x1, y1, x2, y2 = map(int, d[:4])
+                w, h = x2 - x1, y2 - y1
+
+                b_color = (16, 216, 16)
+                cv2.rectangle(f1, (x1, y1), (x2, y2), b_color, thickness=1, lineType=cv2.LINE_AA)
+
+            # image resize
+            orig_img_size = (f0.shape[0], f0.shape[1])
+            edit_img_size = orig_img_size
+            global img
+            img = f1
+            while f1.shape[0] > 1000:
+                f1 = cv2.resize(f1, (int(im_w * 0.8), int(im_h * 0.8)))
+                img = f1
+                edit_img_size = (f1.shape[0], f1.shape[1])
+            cv2.imshow(winname, f1)
+
+            auto_box = 0
+            for _idx, d in enumerate(_det):
                 x1, y1, x2, y2 = map(int, d[:4])
                 w, h = x2 - x1, y2 - y1
                 _cls_idx = int(d[5])
+                crop_image = f0[y1:y2, x1:x2]
+                b_name = f"{_idx+1}/{len(_det)}"
+                cv2.imshow(b_name, crop_image)
 
-                b_color = (216, 16, 16) if _cls_idx == 1 else (16, 16, 224)  # 0: female 1: male
-                cv2.rectangle(f1, (x1, y1), (x2, y2), b_color, thickness=2, lineType=cv2.LINE_AA)
+                nx1, ny1 = cvtPoint(x1, y1, orig_img_size, edit_img_size)
+                nx2, ny2 = cvtPoint(x2, y2, orig_img_size, edit_img_size)
+
+                _k = cv2.waitKey(0)
+                if _k == ord('q'):
+                    get_logger().info("-- CV2 Stop --")
+                    is_out = True
+                    cv2.destroyWindow(b_name)
+                    break
+                elif _k == ord('1'):
+                    category_id = 1
+                    b_color = (216, 16, 16)  # male blue
+                elif _k == ord('2'):
+                    category_id = 2
+                    b_color = (16, 16, 224)  # female red
+                else:
+                    cv2.destroyWindow(b_name)
+                    continue
+                auto_box += 1
+                cv2.rectangle(f1, (nx1, ny1), (nx2, ny2), b_color, thickness=1, lineType=cv2.LINE_AA)
+                cv2.imshow(winname, f1)
+                cv2.destroyWindow(b_name)
 
                 anno_info = {"id": anno_ids,
                              "image_id": img_ids,
-                             "category_id": _cls_idx,
+                             "category_id": category_id,
                              "bbox": [x1, y1, w, h],
                              "area": w * h,
                              "segmentation": [],
                              "iscrowd": 0}
                 tmp_annos.append(anno_info)
                 anno_ids += 1
-                c_id[_cls_idx] += 1
-
-            while f1.shape[0] > 1000:
-                f1 = cv2.resize(f1, (int(im_w * 0.8), int(im_h * 0.8)))
-            cv2.imshow(winname, f1)
 
             k = cv2.waitKey(0)
             cv2.destroyAllWindows()
@@ -146,28 +244,54 @@ def main(opt=None):
                 get_logger().info("-- CV2 Stop --")
                 is_out = True
                 break
-            elif k == ord('y'):
-                pass
-            elif k == ord('n'):
-                continue
-            else:
-                continue
+            elif k == ord(" "):
+                global label_info
+                for l_info in label_info:
+                    pt1, pt2 = l_info[0], l_info[1]
+                    rel_pt1 = (pt1[0] / edit_img_size[1], pt1[1] / edit_img_size[0])
+                    rel_pt2 = (pt2[0] / edit_img_size[1], pt2[1] / edit_img_size[0])
+                    orig_pt1 = (int(rel_pt1[0] * orig_img_size[1]), int(rel_pt1[1] * orig_img_size[0]))
+                    orig_pt2 = (int(rel_pt2[0] * orig_img_size[1]), int(rel_pt2[1] * orig_img_size[0]))
+                    w = int(orig_pt2[0] - orig_pt1[0])
+                    h = int(orig_pt2[1] - orig_pt1[1])
 
-            if is_out is False:
-                for k, v in c_id.items():
-                    obj_classes[k] += v
-                for _anno_info in tmp_annos:
-                    basic_fmt["annotations"].append(_anno_info)
+                    anno_info = {
+                        "id": anno_ids,
+                        "image_id": img_ids,
+                        "category_id": l_info[2],
+                        "bbox": [orig_pt1[0], orig_pt1[1], w, h],
+                        "area": w * h,
+                        "segmentation": [],
+                        "iscrowd": 0
+                    }
+                    anno_ids += 1
+                    tmp_annos.append(anno_info)
+                    c_id[l_info[2]] += 1
 
-                basic_fmt["images"].append(img_info)
+                if is_out is False:
+                    basic_fmt["images"].append(img_info)
+                    for k, v in c_id.items():
+                        obj_classes[k] += v
+                    for _anno_info in tmp_annos:
+                        basic_fmt["annotations"].append(_anno_info)
+                    get_logger().info(
+                        f"Save label {img_file}. Add {len(label_info) + auto_box} boxes."
+                    )
+                label_info = []
+
                 new_path = os.path.join(IMGS_DIR, opt.type, i)
-                # if not os.path.exists(os.path.dirname(new_path)):
-                #     os.makedirs(os.path.dirname(new_path))
-                # shutil.move(img_file, new_path)
-            img_ids += 1
+                if not os.path.exists(os.path.dirname(new_path)):
+                    os.makedirs(os.path.dirname(new_path))
+                shutil.move(img_file, new_path)
+                img_ids += 1
+            else:
+                global label_info
+                label_info = []
+                continue
 
-    # with open(os.path.join(opt.json_file), 'w') as outfile:
-    #     json.dump(basic_fmt, outfile, indent=2)
+    with open(os.path.join(opt.json_file), 'w') as outfile:
+        ensure_ascii = False if platform.system() == 'Windows' else True
+        json.dump(basic_fmt, outfile, indent=2, ensure_ascii=ensure_ascii)
 
     get_logger().info(f"obj classes: {obj_classes}")
 

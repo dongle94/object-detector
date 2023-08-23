1e668d4c83317ea0fbaeab126170a6394aa226ef
dongle94 <sdy1029@gmail.com>
2023-08-23 19:16:38 +0900

    Feat: dv4 annotate - 반자동 라벨링 2차까지 학습한 모델을 기반으로 성별 클래스에 따라 남성 객체는 파란색, 여성 객체는 빨강색으로 표시 - 문제가 없다면 스페이스바를 눌러 라벨링에 추가하고 따로 보관할 파일은 n을 눌러 pending 디렉토리에 보관. 일단 넘기려면 그 외의 키보드 입력

--- dv/dv4_annotate.py
+++ dv/dv4_annotate.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 """
 datavoucher 4 - withyou
 human object classes by gender: male, female
-yolov5 x6 model + homogenus(gender classification) auto labeling version
+custom yolo model(x6) auto labeling version
 """
 import json
 import os
@@ -11,6 +12,8 @@ from datetime import datetime
 from collections import defaultdict
 import cv2
 import shutil
+import numpy as np
+import platform
 
 from pathlib import Path
 FILE = Path(__file__).resolve()
@@ -25,8 +28,9 @@ from utils.logger import get_logger, init_logger
 from core.obj_detectors import ObjectDetector
 
 def main(opt=None):
-    IMGS_DIR = opt.imgs_dir
-    get_logger().info(f"Input Directory is {IMGS_DIR}")
+    get_logger().info(f"Start dv4 annotation script.")
+    IMGS_DIRS = opt.imgs_dir
+    get_logger().info(f"Input Directory is {IMGS_DIRS}")
 
     detector = ObjectDetector(cfg=cfg)
     obj_classes = defaultdict(int)
@@ -36,10 +40,10 @@ def main(opt=None):
             basic_fmt = json.load(file)
         get_logger().info(f"{opt.json_file} is exist. append annotation file.")
         img_ids = 0
+        anno_ids = 0
         if len(basic_fmt['images']) != 0:
             img_ids = int(basic_fmt['images'][-1]['id']) + 1
             get_logger().info(f"last image file name: {basic_fmt['images'][-1]['file_name']}")
-        anno_ids = 0
         if len(basic_fmt['annotations']) != 0:
             anno_ids = int(basic_fmt["annotations"][-1]['id']) + 1
             for anno in basic_fmt['annotations']:
@@ -49,13 +53,14 @@ def main(opt=None):
         get_logger().info(f"{opt.json_file} is not exist. Create new annotation file")
         basic_fmt = {
             "info": {"year": "2023", "version": "1",
-                     "description": "",
+                     "description": "datavoucher human gender object detection dataset.",
                      "contributor": "",
                      "url": "",
                      "date_created": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")},
             "licenses": [{"id": 1, "url": "", "name": "Unknown"}],
-            "categories": [{"id": 0, "name": "male", "supercategory": "person"},
-                           {"id": 1, "name": "female", "supercategory": "person"}],
+            "categories": [{"id": 0, "name": "background", "supercategory": "background"},
+                           {"id": 1, "name": "male", "supercategory": "person"},
+                           {"id": 2, "name": "female", "supercategory": "person"}],
             "images": [],
             "annotations": []
         }
@@ -64,98 +69,124 @@ def main(opt=None):
 
     is_out = False
 
-    IMGS = os.listdir(IMGS_DIR)
-    IMGS.sort()
-    for i in IMGS:
+    image_extension = ['.jpg', '.png', '.jpeg', '.bmp']
+    for IMGS_DIR in IMGS_DIRS:
         if is_out is True:
             break
-        if os.path.splitext(i)[-1].lower() not in ['.jpg', '.png', '.jpeg', '.bmp']:
-            continue
 
-        img_file = os.path.join(IMGS_DIR, i)
-        get_logger().info(f"process {img_file}.")
-        f = cv2.imread(img_file)
+        IMGS = [i for i in os.listdir(IMGS_DIR) if os.path.splitext(i)[-1].lower() in image_extension]
+        IMGS.sort()
+        get_logger().info(f"process {IMGS_DIR} ..")
+        for idx, i in enumerate(IMGS):
+            if is_out is True:
+                break
 
-        # yolov5 human detector
-        im = detector.preprocess(f)
-        _pred = detector.detect(im)
-        _pred, _det = detector.postprocess(_pred)
+            img_file = os.path.join(IMGS_DIR, i)
+            get_logger().info(f"process {img_file}.")
+            f0 = cv2.imread(img_file)
+            if os.path.exists(img_file) is True and f0 is None:      # File 경로에 한글
+                f0 = open(img_file.encode("utf8"), mode="rb")
+                bs = bytearray(f0.read())
+                arr = np.asarray(bs, dtype=np.uint8)
+                f0 = cv2.imdecode(arr, cv2.IMREAD_UNCHANGED)
 
-        img_info = {
-            "id": img_ids,
-            "license": 1,
-            "file_name": i,
-            "height": f.shape[0],
-            "width": f.shape[1],
-            "data_captured": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-        }
+            winname = f"{idx + 1}/{len(IMGS)}"
 
-        # bbox annotation
-        c_id = defaultdict(int)
-        tmp_annos = []
-        for d in _det:
-            if d[4] < float(opt.confidence):
-                continue
-            x1, y1, x2, y2 = map(int, d[:4])
-            w, h = x2-x1, y2-y1
-            fc = f.copy()
-            cv2.rectangle(fc, (x1, y1), (x2, y2), (16, 16, 255), thickness=2, lineType=cv2.LINE_AA)
+            # yolov5 human detector
+            im = detector.preprocess(f0)
+            _pred = detector.detect(im)
+            _pred, _det = detector.postprocess(_pred)
 
-            if fc.shape[0] > 1000:
-                fc = cv2.resize(fc, (int(fc.shape[1] * 0.8), int(fc.shape[0] * 0.8)))
-            cv2.imshow(img_file, fc)
+            img_info = {
+                "id": img_ids,
+                "license": 1,
+                "file_name": os.path.join(opt.type, i),
+                "height": f0.shape[0],
+                "width": f0.shape[1],
+                "data_captured": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+            }
 
-            category_id = 0
+            # bbox annotation
+            c_id = defaultdict(int)
+            tmp_annos = []
+
+            f1 = f0.copy()
+            for d in _det:
+                x1, y1, x2, y2 = map(int, d[:4])
+                w, h = x2 - x1, y2 - y1
+
+                if int(d[5]) == 0:      # female
+                    b_color = (16, 16, 216)
+                    category_id = 2
+                elif int(d[5]) == 1:    # male
+                    b_color = (216, 16, 16)
+                    category_id = 1
+                else:                   # bug
+                    raise "wrong index class id"
+
+                anno_info = {"id": anno_ids,
+                             "image_id": img_ids,
+                             "category_id": category_id,
+                             "bbox": [x1, y1, w, h],
+                             "area": w * h,
+                             "segmentation": [],
+                             "iscrowd": 0}
+                tmp_annos.append(anno_info)
+                anno_ids += 1
+                c_id[category_id] += 1
+
+                cv2.rectangle(f1, (x1, y1), (x2, y2), b_color, thickness=1, lineType=cv2.LINE_AA)
+
+            while f1.shape[0] >= 1080:
+                f1 = cv2.resize(f1, (int(f1.shape[1] * 0.9), int(f1.shape[0] * 0.9)))
+            cv2.imshow(winname, f1)
+
             k = cv2.waitKey(0)
-            if k == ord('q'):
-                get_logger().info("-- CV2 Stop --")
+            cv2.destroyAllWindows()
+            if k == ord("q"):
+                get_logger().info("-- Click q key, CV2 Stop --")
                 is_out = True
                 break
-            elif k == ord('1'):
-                category_id = 0
-            elif k == ord('2'):
-                category_id = 1
-            elif k == ord('d'):
+            elif k == ord(" "):
+                basic_fmt["images"].append(img_info)
+                img_ids += 1
+                for k, v in c_id.items():
+                    obj_classes[k] += v
+                for _anno_info in tmp_annos:
+                    basic_fmt["annotations"].append(_anno_info)
+                get_logger().info(f"Save label {img_file}. Add {len(_det)} boxes.")
+
+                new_path = os.path.join(IMGS_DIR, opt.type, i)
+                if not os.path.exists(os.path.dirname(new_path)):
+                    os.makedirs(os.path.dirname(new_path))
+                shutil.move(img_file, new_path)
+            elif k == ord("n"):
+                get_logger().info(f"Pending this image. move {img_file} to pending directory.")
+                new_path = os.path.join(IMGS_DIR, "pending", i)
+                if not os.path.exists(os.path.dirname(new_path)):
+                    os.makedirs(os.path.dirname(new_path))
+                shutil.move(img_file, new_path)
+            else:
+                get_logger().info(f"Pass this image({i}). you will see again this image when you label this dir again.")
                 continue
 
-            anno_info = {"id": anno_ids,
-                         "image_id": img_ids,
-                         "category_id": category_id,
-                         "bbox": [x1, y1, w, h],
-                         "area": w*h,
-                         "segmentation": [],
-                         "iscrowd": 0}
-            tmp_annos.append(anno_info)
-            anno_ids += 1
-            c_id[category_id] += 1
-
-        cv2.destroyAllWindows()
-        if is_out is False:
-            for k, v in c_id.items():
-                obj_classes[k] += v
-            for _anno_info in tmp_annos:
-                basic_fmt["annotations"].append(_anno_info)
-
-            basic_fmt["images"].append(img_info)
-            new_path = os.path.join(IMGS_DIR, 'already', i)
-            if not os.path.exists(os.path.dirname(new_path)):
-                os.makedirs(os.path.dirname(new_path))
-            shutil.move(img_file, new_path)
-        img_ids += 1
-
     with open(os.path.join(opt.json_file), 'w') as outfile:
-        json.dump(basic_fmt, outfile, indent=2)
+        ensure_ascii = False if platform.system() == 'Windows' else True
+        json.dump(basic_fmt, outfile, indent=2, ensure_ascii=ensure_ascii)
+
     get_logger().info(f"obj classes: {obj_classes}")
 
 
 def args_parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--imgs_dir', required=True,
+    parser.add_argument('-i', '--imgs_dir', required=True, nargs='+',
                         help='image directory path')
     parser.add_argument('-j', '--json_file', required=True,
                         help="if write this file, append annotations")
-    parser.add_argument('-c', '--confidence', default=0.3,
-                        help="obj_detector confidence")
+    parser.add_argument('-t', '--type', default='train',
+                        help='type is in [train, val]. this option write file_path {type}/img_file')
+    parser.add_argument('-c', '--config', default='./configs/annotate.yaml',
+                        help="annotation configuration yaml file path")
     _args = parser.parse_args()
     return _args
 
@@ -163,7 +194,7 @@ def args_parse():
 if __name__ == "__main__":
     args = args_parse()
 
-    update_config(cfg, args='./configs/annotate.yaml')
+    update_config(cfg, args=args.config)
     init_logger(cfg)
 
     main(args)

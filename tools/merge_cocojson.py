f9ebdb3087e9c2b4d0abfd0ea41f5c9b62fe1463
dongle94 <sdy1029@gmail.com>
2023-09-27 18:22:46 +0900

    Fixed: merge_cocojson.py - 불필요한 루프 줄이기 - 코드 가독성 개선 및 알고리즘 개선

--- tools/merge_cocojson.py
+++ tools/merge_cocojson.py
@@ -3,8 +3,8 @@
 COCO label json file merge tool
 """
 
-import json
 import os
+import json
 import copy
 import argparse
 
@@ -16,7 +16,6 @@ def getKeybyValue(cls_dict, idx):
 
 
 def main(opt):
-
     inputs_json = opt.inputs
     if len(inputs_json) == 0:
         print("No input json files")
@@ -38,40 +37,43 @@ def main(opt):
         print("Meta json file not exist")
         return
 
-    imageid_map = {}
     for input_json in inputs_json:
-        if os.path.exists(input_json):
-            print(f"Start {input_json}")
-            with open(input_json, mode='r', encoding='utf8') as file:
-                json_dict = json.load(file)
-            for json_img in json_dict['images']:
-                imageid_map[img_ids] = json_img['file_name']
-                img_json = copy.deepcopy(json_img)
-                img_json['id'] = img_ids
-                basic_fmt['images'].append(img_json)
-                img_ids += 1
+        if not os.path.exists(input_json):
+            print(f"No exist {input_json}")
+            continue
 
-            for json_anno in json_dict['annotations']:
-                local_img_id = json_anno['image_id']
-                image_id = 0
-                for json_img in json_dict['images']:
-                    if json_img['id'] == local_img_id:
-                        image_id = getKeybyValue(imageid_map, json_img['file_name'])
-                        break
-                json_anno['id'] = anno_ids
-                json_anno['image_id'] = image_id
-                basic_fmt['annotations'].append(json_anno)
-                anno_ids += 1
+        print(f"Start {input_json}")
+        imageid_map = {}
+        with open(input_json, mode='r', encoding='utf8') as file:
+            json_dict = json.load(file)
+        # Process(Merge) images field
+        for json_img in json_dict['images']:
+            imageid_map[img_ids] = json_img['id']
+            img_json = copy.deepcopy(json_img)
+            img_json['id'] = img_ids
+            basic_fmt['images'].append(img_json)
+            img_ids += 1
 
-            print(f"End {input_json}")
+        # Process(Merge) annotations field
+        for json_anno in json_dict['annotations']:
+            orig_img_id = json_anno['image_id']
+            new_img_id = getKeybyValue(imageid_map, orig_img_id)
+
+            anno_json = copy.deepcopy(json_anno)
+            anno_json['id'] = anno_ids
+            anno_json['image_id'] = new_img_id
+            basic_fmt['annotations'].append(anno_json)
+            anno_ids += 1
+
+        print(f"End {input_json}")
 
     with open(opt.output, 'w') as outfile:
-        json.dump(basic_fmt, outfile, indent=2, ensure_ascii=False)
-    print("Merge success. stop program.")
+        json.dump(basic_fmt, outfile, indent=1, ensure_ascii=False)
+    print(f"Merge success to {opt.output}. stop program.")
 
 
 def args_parse():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="First input json's metadata is leading whole metadata.")
     parser.add_argument('-i', '--inputs', required=True, nargs='+',
                         help='input json file path, use 1st input json metadata(info, license, categories)')
     parser.add_argument('-o', '--output', required=True,

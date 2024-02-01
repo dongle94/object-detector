6acc160c0c48ca14a0a02ec9dd68a6dbc3c86f7c
dongle94 <sdy1029@gmail.com>
2024-02-01 12:52:20 +0900

    Feat:  YOLOV5 원형
    - __init__.py 를 활용하여 해당 클래스의 기본형 도입
    - 프레임워크(파이토치, onnxruntime) 에 따른 상속 구현 예정

---
+++ core/yolov5/__init__.py
@@ -0,0 +1,17 @@
+
+
+class YOLOV5(object):
+    def __init__(self):
+        pass
+
+    def warmup(self):
+        pass
+
+    def preprocess(self):
+        pass
+
+    def infer(self):
+        pass
+
+    def postprocess(self):
+        pass

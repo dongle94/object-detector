6026c973a2154f1803a6c29983eb9d095d4f29d0
dongle94 <sdy1029@gmail.com>
2024-02-19 22:44:38 +0900

    Feat: MP_Process - 홈 코딩

--- core/mp/mp_queue.py
+++ core/mp/mp_queue.py
@@ -1,14 +1,21 @@
 from queue import Queue
+from threading import Lock
+
 
 class MessageQueue(object):
 
-    def __init__(self):
-
-        self.queue = Queue(maxsize=0)
+    def __init__(self, q_size=0):
+        self.queue = Queue(maxsize=q_size)
+        self.lock = Lock()
 
     def put(self, data):
         self.queue.put(data)
 
-    def __repr__(self):
-        # return self.que
-        pass
+    def get(self, block=True, timeout=None):
+        """
+        if block is False, timeout ignore
+        """
+        with self.lock:
+            data = self.queue.get(block=block, timeout=timeout)
+
+        return data
--- core/mp/task.py
+++ core/mp/task.py
@@ -1,2 +1,13 @@
 
 from multiprocessing import Queue, Process
+
+
+class MPProcess(Process):
+    def __int__(self, name):
+        super().__init__()
+
+        self.name = name
+
+
+    def run(self):
+        print(self.name)

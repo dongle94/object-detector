bebb81aed286f7c49890883b49efde631a1cf730
dongle94 <sdy1029@gmail.com>
2024-04-15 11:19:17 +0900

    Feat: yolov8 model 초안
    - yolov5와 비슷하도록 yolov8 모델 추상 클래스 제작

--- core/yolov8/__init__.py
+++ core/yolov8/__init__.py
@@ -2,6 +2,27 @@
 
 __version__ = "8.1.0"
 
-from core.yolov8.models import YOLOV8
+import time
+# from core.yolov8.models import YOLOV8
 
-__all__ = "__version__", "YOLOV8"
+__all__ = "__version__", "YOLOv8", #"YOLOV8"
+
+
+class YOLOv8(object):
+    def __init__(self):
+        pass
+
+    def warmup(self, img_size):
+        pass
+
+    def preprocess(self, img):
+        pass
+
+    def infer(self, img):
+        pass
+
+    def postprocess(self, pred, im_shape, im0_shape):
+        pass
+
+    def get_time(self):
+        return time.time()

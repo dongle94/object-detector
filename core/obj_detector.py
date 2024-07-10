ce0ffcc96f638be10ffe63918d18ee1be3a3acb5
dongle94 <sdy1029@gmail.com>
2024-07-11 01:31:13 +0900

    Feat: yolov10
    - yolov10_pt.py, obj_detector.py
    - pytorch 모듈은 가동되는 것 확인, obj_detector는 최적화가 필요한 것으로 보임

--- core/obj_detector.py
+++ core/obj_detector.py
@@ -26,7 +26,7 @@ class ObjectDetector(object):
 
         self.framework = None
 
-        if self.detector_type in ["yolov5", "yolov8"]:
+        if self.detector_type in ["yolov5", "yolov8", "yolov10"]:
             img_size = cfg.yolov5_img_size
             iou_thres = cfg.yolov5_nms_iou
             agnostic = cfg.yolov5_agnostic_nms
@@ -67,6 +67,12 @@ class ObjectDetector(object):
                     self.framework = 'trt'
                 else:
                     raise FileNotFoundError('No Yolov8 weight File!')
+            elif self.detector_type == "yolov10":
+                ext = os.path.splitext(weight)[1]
+                if ext in ['.pt', '.pth']:
+                    from core.yolov10.yolov10_pt import Yolov10Torch
+                    model = Yolov10Torch
+                    self.framework = 'torch'
             else:
                 raise NotImplementedError(f'Unknown detector type: {self.detector_type}')
             self.detector = model(
@@ -92,7 +98,7 @@ class ObjectDetector(object):
         self.ts = [0., 0., 0.]
 
     def run(self, img):
-        if self.detector_type in ["yolov5", "yolov8"]:
+        if self.detector_type in ["yolov5", "yolov8", "yolov10"]:
             t0 = self.detector.get_time()
 
             img, orig_img = self.detector.preprocess(img)

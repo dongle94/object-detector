eb1c711a984e19f1de14895560fd83bc82cd12ab
dongle94 <sdy1029@gmail.com>
2024-11-14 16:04:45 +0900

    Docs: YOLO warmup
    - yolo_pt.py, yolo_ort.py, yolo_trt.py
    - warmup 추론을 2번으로 변경

--- core/yolo/yolo_pt.py
+++ core/yolo/yolo_pt.py
@@ -57,8 +57,10 @@ class YoloTorch(YOLO):
         if img_size is None:
             img_size = (1, 3, self.img_size[0], self.img_size[1])
         im = torch.empty(*img_size, dtype=torch.half if self.fp16 else torch.float, device=self.device)
+
         t = self.get_time()
-        self.infer(im)
+        for _ in range(2):
+            self.infer(im)  # warmup
         self.logger.info(f"-- {self.kwargs['model_type']} Pytorch Detector warmup: {time.time()-t:.6f} sec --")
 
     def preprocess(self, img):
